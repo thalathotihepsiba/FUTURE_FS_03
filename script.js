62141c960de16ba4ef1978684acc6ca3a1888367
thalathotihepsiba <thalathotihepsiba@gmail.com>
2026-07-24 12:37:10 +0530

    script.js

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
 function scrollToContact(){
 
-    document
-    .getElementById("contact")
-    .scrollIntoView({
-        behavior:"smooth"
-    });
+document
+.getElementById("contact")
+.scrollIntoView({
+
+behavior:"smooth"
+
+});
 
 }
 
 
-document.querySelectorAll("nav a")
+
+document
+.querySelectorAll("nav a")
 .forEach(link=>{
+
 
 link.addEventListener("click",function(){
 
-console.log("Section opened");
+console.log("Navigation clicked");
 
 });
+
+
+});
+
+
+
+document
+.querySelector("form")
+.addEventListener("submit",function(e){
+
+e.preventDefault();
+
+
+alert(
+"Thank you for contacting Hepsi Café!"
+);
+
+
+this.reset();
+
 
 });
